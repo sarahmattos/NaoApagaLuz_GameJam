00b6d065c514ed4b0fb9a205441d9b1894317527
Sarah Mattos <sahmattos13@gmail.com>
2024-12-23 19:03:08 -0300

    menu buttons

--- NãoApagaALuz/Assets/Scripts/GameManager/MenuManager.cs
+++ NãoApagaALuz/Assets/Scripts/GameManager/MenuManager.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject creditsPanel;
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene("TesteArte");
+    }
+    public void ShowCredits()
+    {
+        creditsPanel.SetActive(!creditsPanel.activeSelf);
     }
 }
